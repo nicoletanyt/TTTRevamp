16b8348e58b567941f7ac78da668f11655d8910b
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-10-04 23:43:08 +0800

    added banner slideshow

--- ttt_revamp/src/homepage/Homepage.js
+++ ttt_revamp/src/homepage/Homepage.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Footnote from './Footnote';
 import Timeline from './Timeline';
 
+// Import Images
+import Banner1 from "../assets/Banner1.png"
+import Banner2 from "../assets/Banner2.png";
+import Banner3 from "../assets/Banner3.png";
+const BANNERS = [Banner1, Banner2, Banner3];
+
 export default function Homepage() {
+  const [bannerImg, setBannerImg] = useState(0);
+
+  useEffect(() => {
+    const imgInterval = setInterval(() => {
+      setBannerImg((prevBannerImg) => (prevBannerImg + 1) % BANNERS.length);
+    }, 3000);
+    return () => clearInterval(imgInterval);
+  }, [])
+
   return (
     <div id="homepage">
+      <img src={BANNERS[bannerImg]} alt="Banner" />,
       <section>
         <p>Key messages</p>
       </section>
